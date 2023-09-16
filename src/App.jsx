33de3dcc5b82d5ce8d81098c9dcb4f21ac590a28
Chung-Ying <ag.cyho@gmail.com>
2023-09-16 21:29:42 +0800

    feat: add gh-page

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import './App.scss';
-import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import MainLayout from './layouts/Main/MainLayout';
 import Home from './pages/Home/Home';
 import Skill from './pages/Skill/Skill';
 import Portfolio from './pages/Portfolio/Portfolio';
 import About from './pages/About/About';
 
+const basename = process.env.PUBLIC_URL;
+
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <BrowserRouter basename={basename}>
           <Routes>
               <Route element={<MainLayout />}>
                   <Route path='/' element={<Home/>}></Route>
